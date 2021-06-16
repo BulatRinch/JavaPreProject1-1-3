272b337940aa4c55cb256e96c8515baf99c54742
BulatRinch <rinchb@internet.ru>
2021-06-16 21:32:40 +0800

    1-1-3

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -2,6 +2,11 @@ package jm.task.core.jdbc;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("123");
+        System.out.println("12345");
+        System.out.println("12345");
+        System.out.println("12345");
+        System.out.println("12345");
+        System.out.println("12345");
+        System.out.println("12345");
     }
 }
